6471335958fc2e22d10ece36291e48d53da81212
adamTheWordUnderscoreWillis <adamthewordunderscorewillis@gmail.com>
2024-06-17 14:33:50 +0100

    Lays out all articles on a home page

--- src/api.js
+++ src/api.js
@@ -4,3 +4,9 @@ const newsApi = axios.create({
     baseURL: "https://adam-willis-cool-news-hub.onrender.com",
 });
 
+export const GetArticles = ()=> {
+    return newsApi.get("/api/articles").then((res)=>{
+        return res.data;
+    })
+}
+
--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react"
+import { GetArticles } from "../api"
 const Articles =()=>{
+const [articles, setArticles] = useState([])
+const [isLoading, setIsLoading] = useState(true)
+
+useEffect(()=>{
+    setIsLoading(true)
+    GetArticles().then(({articles})=>{
+        setArticles(articles);
+        setIsLoading(false);
+        })
+
+}, [])
+ console.log(articles)
     return (
         <div className="Articles">
-            <h1>Articles</h1>
+            <h2>Coolest New Articles</h2>
+            <section className="articleHolder">
+                {articles.map((article)=>{
+                    return(
+                    <div key={article.article_id} className = "articleCard">
+                        <img className="articleImg" src={article.article_img_url} alt={article.title} />
+                        <h3 className="cardTitle">{article.title}</h3>
+                        <p className="classTopic">{article.topic}</p>
+                    </div>
+                    )
+
+                })
+                }
+            </section>
         </div>
 )
 }
