f2ce3a2e21d22aac6b86013a5315393089ecd512
adamTheWordUnderscoreWillis <adamthewordunderscorewillis@gmail.com>
2024-06-18 09:35:43 +0100

    Adds all comments to articles

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,32 +1,58 @@
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
-import { GetArticleById } from "../api"
+import { GetArticleById, getCommentsByArticleID } from "../api"
 const ArticleById =()=>{
 const [selectedArticle , setSelectedArticle] = useState("")
+const [articleComments, setArticleComments] = useState([])
+const [isLoading, setIsLoading] = useState(true)
 const {articleId} = useParams()
 useEffect(()=>{
-    GetArticleById(articleId).then(({article})=>{
+    setIsLoading(true)
+    GetArticleById(articleId)
+    .then(({article})=>{
         setSelectedArticle(article)
         })
+    getCommentsByArticleID(articleId)
+    .then(({comments})=>{
+        setArticleComments(comments)
+    })
+    setIsLoading(false)
+
 }, [articleId])
 
-console.log(selectedArticle)
+if(isLoading){
+    return (<h3>Give me a second... for christ sake...</h3>)
+    
+}
     return (
-        <article >
-            <div className="articleItem">
-                <h2>{selectedArticle.title}</h2>
-                <h3> by {selectedArticle.author}</h3>
-                <h4>{selectedArticle.topic}</h4>
-            <p>{selectedArticle.body}</p>
+        <>
+            <article >
+                <div className="articleItem">
+                    <h2>{selectedArticle.title}</h2>
+                    <h3> by {selectedArticle.author}</h3>
+                    <h4>{selectedArticle.topic}</h4>
+                <p>{selectedArticle.body}</p>
 
-            </div>
-            <div className="articleItem">
-                <img src={selectedArticle.article_img_url} alt={selectedArticle.title} />
-            </div>
+                </div>
+                <div className="articleItem">
+                    <img src={selectedArticle.article_img_url} alt={selectedArticle.title} />
+                </div>
+            </article>
+            <ul className="commentSection">
+                {articleComments.map((comment)=>{
+                    return (<li className="commentCard">
+                        <header className="commentHeader">
+                            <h5 className="commentHeaderAuthor">{comment.author}</h5>
+                            <p className="commentHeaderDate">{comment.created_at}</p>
+                        </header>
+                        <p className="commentBody">{comment.body}</p>
+                        <p className="commentVote">Votes: {comment.votes}</p>
 
+                    </li>)
 
-            
-        </article>
+                })}
+            </ul>
+        </>
 )
 }
 export default ArticleById
